3bb5a8bc4bbb54c9e0ddc3f8b9b097ca3981fa96
ma01talleb@gmail.com <abwsyafalhathmy63@gmail.com>
2024-03-03 03:59:19 -0500

    updates 1

--- src/App.js
+++ src/App.js
@@ -17,20 +17,7 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 const firebase = initializeApp({
-  apiKey: "AIzaSyADwJ6rQG7Iil19mk3K7f1A_lg-RHo58KI",
-
-  authDomain: "ak-chat-server.firebaseapp.com",
-
-  projectId: "ak-chat-server",
-
-  storageBucket: "ak-chat-server.appspot.com",
-
-  messagingSenderId: "368720793781",
-
-  appId: "1:368720793781:web:b640cf023c74ed16936efc",
-
-  measurementId: "G-RQF1CPLTE3",
-  error: true,
+  // config
 })
 
 const auth = getAuth(firebase);
@@ -45,7 +32,10 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>⚛️🔥💬</h1>
+        <h1>
+          <img alt='' src='/favicon.ico'/>
+           AK-CHAT-SERVER <i class='fa fa-comments'></i>
+        </h1>
         <SignOut />
       </header>
 
@@ -64,7 +54,13 @@ function SignIn() {
     signInWithPopup(auth, provider)
       .then((result) => {
         // Handle successful sign-in (e.g., navigate to a different page)
-        console.log("Signed in successfully:", result.user);
+        alert(`Welcome to the server, AK-CHAT-SERVER welcomes all to chat and share updates, but respectful behavior is expected:
+
+        Treat others kindly, AND avoid abusive language.
+        Stick to the topic of AK01REDWAN's news and updates.
+        Report any violations to moderators.
+
+        Failure to follow these guidelines may result in warnings, suspension, or ban.`);
       })
       .catch((error) => {
         // Handle sign-in errors
@@ -75,11 +71,13 @@ function SignIn() {
 
   return (
     <>
-      <button className="sign-in" onClick={signInWithGooglePopup}>Sign in with Google</button>
+      <button className="sign-in" onClick={signInWithGooglePopup}>
+        <i class='fa fa-google-plus'></i> Sign-in with Google sign-out <i class="fa fa-sign-in"></i>
+        </button>
       <p>
         <strong>AK-CHAT-SERVER</strong> welcomes all to chat and share updates, but respectful behavior is expected:
 
-        Treat others kindly, avoid abusive language and hate speech.
+        Treat others kindly, AND avoid abusive language.
         Stick to the topic of AK01REDWAN's news and updates.
         Report any violations to moderators.
 
@@ -94,7 +92,7 @@ function SignIn() {
 
 function SignOut() {
   return auth.currentUser && (
-    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
+    <button className="sign-out" onClick={() => auth.signOut()}>SignOut <i class="fa fa-sign-out"></i></button>
   )
 }
 
@@ -135,9 +133,9 @@ function ChatRoom() {
 
     <form onSubmit={sendMessage}>
 
-      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
+      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="Write something .." />
 
-      <button type="submit" disabled={!formValue}>SEND</button>
+      <button type="submit" disabled={!formValue}><i class="fa fa-send-o"></i></button>
 
     </form>
   </>)
